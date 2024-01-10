308b7f5775f11c7023446f0a9e5a2eb0d44c4d59
TanjibQuad <tanjib@quadque.tech>
2024-01-10 09:35:51 +0600

    change status api fix

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -76,7 +76,7 @@ class ReminderController extends Controller
             return response()->json([
                 'message' => 'success',
                 'status' => 200,
-                'data' => 'You have a meeting at after 10 minutes at' .' '. $time
+                'data' => 'You have a meeting at after 10 minutes at' . ' ' . $time
             ]);
         } else {
             return response()->json([
@@ -104,61 +104,61 @@ class ReminderController extends Controller
      */
     public function store(Request $request)
     {
-        if($request->bearerToken()){
-                $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
-                $flag_receive = $flag['data'];
-                if($flag_receive == 1){
-                    // try {
-                        $request->validate([
-                            'title'=>'required',
-                            'start'=>'required',
-                            'end'=>'required',
-                            'user_id'=>'required',
-                            'notification_time'=>'required',
-                        ]);
-                        $follow_up = FollowUp::create([
-                            'title' => $request->title,
-                            'start' => $request->start,
-                            'end' => $request->end,
-                            'description' => $request->description,
-                            'priority' => $request->priority,
-                            'user_id' => $request->user_id,
-                            'notification_time'=>$request->notification_time,
-                            'status' => 1
-                        ]);
-                        // dd($follow_up);
-                        if ($follow_up) {
-                            return response()->json([
-                                'status' => 201,
-                                'message' => "success",
-                                'data' => $follow_up
-                            ], 201);
-                        } else {
-                            return response()->json([
-                                'status' => 400,
-                                'message' => "failed",
-                                'data' => $follow_up
-                            ], 500);
-                        }
-                    // }
-                    // } catch (\Throwable $th) {
-                    //     return response()->json([
-                    //         'status' => false,
-                    //         'message' => $th->getMessage()
-                    //     ], 500);
-                    // }
-                }else{
+        if ($request->bearerToken()) {
+            $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
+            $flag_receive = $flag['data'];
+            if ($flag_receive == 1) {
+                // try {
+                $request->validate([
+                    'title' => 'required',
+                    'start' => 'required',
+                    'end' => 'required',
+                    'user_id' => 'required',
+                    'notification_time' => 'required',
+                ]);
+                $follow_up = FollowUp::create([
+                    'title' => $request->title,
+                    'start' => $request->start,
+                    'end' => $request->end,
+                    'description' => $request->description,
+                    'priority' => $request->priority,
+                    'user_id' => $request->user_id,
+                    'notification_time' => $request->notification_time,
+                    'status' => 1
+                ]);
+                // dd($follow_up);
+                if ($follow_up) {
                     return response()->json([
-                            'message' => 'Unauthenticated',
-                            'status' => 401
-                        ], 401);
+                        'status' => 201,
+                        'message' => "success",
+                        'data' => $follow_up
+                    ], 201);
+                } else {
+                    return response()->json([
+                        'status' => 400,
+                        'message' => "failed",
+                        'data' => $follow_up
+                    ], 500);
                 }
-            }else{
+                // }
+                // } catch (\Throwable $th) {
+                //     return response()->json([
+                //         'status' => false,
+                //         'message' => $th->getMessage()
+                //     ], 500);
+                // }
+            } else {
                 return response()->json([
-                            'message' => 'Unauthenticated',
-                            'status' => 401
-                        ], 401);
+                    'message' => 'Unauthenticated',
+                    'status' => 401
+                ], 401);
             }
+        } else {
+            return response()->json([
+                'message' => 'Unauthenticated',
+                'status' => 401
+            ], 401);
+        }
     }
 
     /**
@@ -173,32 +173,32 @@ class ReminderController extends Controller
         //         $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
         //         $flag_receive = $flag['data'];
         //         if($flag_receive == 1){
-                    $follow_up = FollowUp::where('user_id', $request->user_id)->get();
-                    if (!$follow_up->isEmpty()) {
-                        return response()->json([
-                            'message' => "success",
-                            "status" => 200,
-                            "data" => $follow_up
-                        ]);
-                    } else {
-                        return response()->json([
-                            'message' => "not found",
-                            "status" => 200,
-                            'data'=>[]
-                        ],200);
-                    }
-            //     }else{
-            //         return response()->json([
-            //                 'message' => 'Unauthenticated',
-            //                 'status' => 401
-            //             ], 401);
-            //     }
-            // }else{
-            //     return response()->json([
-            //                 'message' => 'Unauthenticated',
-            //                 'status' => 401
-            //             ], 401);
-            // }
+        $follow_up = FollowUp::where('user_id', $request->user_id)->get();
+        if (!$follow_up->isEmpty()) {
+            return response()->json([
+                'message' => "success",
+                "status" => 200,
+                "data" => $follow_up
+            ]);
+        } else {
+            return response()->json([
+                'message' => "not found",
+                "status" => 200,
+                'data' => []
+            ], 200);
+        }
+        //     }else{
+        //         return response()->json([
+        //                 'message' => 'Unauthenticated',
+        //                 'status' => 401
+        //             ], 401);
+        //     }
+        // }else{
+        //     return response()->json([
+        //                 'message' => 'Unauthenticated',
+        //                 'status' => 401
+        //             ], 401);
+        // }
     }
 
     /**
@@ -222,49 +222,49 @@ class ReminderController extends Controller
     public function update(Request $request, $id)
     {
         // try {
-            // if($request->bearerToken()){
-            //     $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
-            //     $flag_receive = $flag['data'];
-            //     if($flag_receive == 1){
-                    $follow_up = FollowUp::find($id);
-                    if ($follow_up) {
-                        $follow_up->update(['title'=>$request->title]);
-                        // $follow_up->start = $request->start;
-                        // $follow_up->end = $request->end;
-                        $follow_up->update(['start'=>$request->start]);
-                        $follow_up->update(['end'=>$request->end]);
-                        $follow_up->update(['status'=>$request->status]);
-                        // $follow_up->notification_time = $request->notification_time;
-                        $follow_up->update(['notification_time'=>$request->notification_time]);
-                        $save = $follow_up->save();
-                        // $request->start = "";
-                        // $request->end = "";
-                        // $request->notification_time = "";
-                        if ($save) {
-                            return response()->json([
-                                'status' => 201,
-                                'message' => "success",
-                                'data' => $follow_up
-                            ], 201);
-                        }
-                    } else {
-                        return response()->json([
-                            'status' => 404,
-                            'message' => "not found"
-                        ], 404);
-                    }
-            //     }else{
-            //         return response()->json([
-            //                 'message' => 'Unauthenticated',
-            //                 'status' => 401
-            //             ], 401);
-            //     }
-            // }else{
-            //     return response()->json([
-            //                 'message' => 'Unauthenticated',
-            //                 'status' => 401
-            //             ], 401);
-            // }
+        // if($request->bearerToken()){
+        //     $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
+        //     $flag_receive = $flag['data'];
+        //     if($flag_receive == 1){
+        $follow_up = FollowUp::find($id);
+        if ($follow_up) {
+            $follow_up->update(['title' => $request->title]);
+            // $follow_up->start = $request->start;
+            // $follow_up->end = $request->end;
+            $follow_up->update(['start' => $request->start]);
+            $follow_up->update(['end' => $request->end]);
+            $follow_up->update(['status' => $request->status]);
+            // $follow_up->notification_time = $request->notification_time;
+            $follow_up->update(['notification_time' => $request->notification_time]);
+            $save = $follow_up->save();
+            // $request->start = "";
+            // $request->end = "";
+            // $request->notification_time = "";
+            if ($save) {
+                return response()->json([
+                    'status' => 201,
+                    'message' => "success",
+                    'data' => $follow_up
+                ], 201);
+            }
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => "not found"
+            ], 404);
+        }
+        //     }else{
+        //         return response()->json([
+        //                 'message' => 'Unauthenticated',
+        //                 'status' => 401
+        //             ], 401);
+        //     }
+        // }else{
+        //     return response()->json([
+        //                 'message' => 'Unauthenticated',
+        //                 'status' => 401
+        //             ], 401);
+        // }
         // } catch (\Throwable $th) {
         //     return response()->json([
         //         'status' => false,
@@ -304,89 +304,90 @@ class ReminderController extends Controller
     //         ], 404);
     //     }
     // }
-    
-    public function notification_list(Request $request,$id)
+
+    public function notification_list(Request $request, $id)
     {
         // if($request->bearerToken()){
         //         $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
         //         $flag_receive = $flag['data'];
         //         if($flag_receive == 1){
-                    if($request->timezone == "Asia/Dhaka"){
-                        $data = FollowUp::where('user_id', $id)->where('notification_time', '<=', Carbon::now()->addHour(6)->toDateTimeString())->orderBy('id','desc')->get();
-                    }elseif($request->timezone == "Australia/Sydney"){
-                        $data = FollowUp::where('user_id', $id)->where('notification_time', '<=', Carbon::now()->addHour(10)->toDateTimeString())->orderBy('id','desc')->get();
-                    }
-                
-                    if (count($data) > 0) {
+        if ($request->timezone == "Asia/Dhaka") {
+            $data = FollowUp::where('user_id', $id)->where('notification_time', '<=', Carbon::now()->addHour(6)->toDateTimeString())->orderBy('id', 'desc')->get();
+        } elseif ($request->timezone == "Australia/Sydney") {
+            $data = FollowUp::where('user_id', $id)->where('notification_time', '<=', Carbon::now()->addHour(10)->toDateTimeString())->orderBy('id', 'desc')->get();
+        }
+
+        if (count($data) > 0) {
+            return response()->json([
+                'message' => 'success',
+                'status' => 200,
+                'data' => $data
+            ], 200);
+        } elseif ($data->isEmpty()) {
+            return response()->json([
+                'message' => 'No data found',
+                'status' => 200,
+                'data' => []
+            ], 200);
+        }
+        //     }else{
+        //         return response()->json([
+        //                 'message' => 'Unauthenticated',
+        //                 'status' => 401
+        //             ], 401);
+        //     }
+        // }else{
+        //     return response()->json([
+        //                 'message' => 'Unauthenticated',
+        //                 'status' => 401
+        //             ], 401);
+        // }
+
+    }
+
+    public function change_status(Request $request, $id)
+    {
+        if ($request->bearerToken()) {
+            $user_base_url = env('USER_BASE_URL');
+            $flag = Http::withToken($request->bearerToken())->post($user_base_url . 'api/check-if-token-exists');
+            $flag_receive = $flag['data'];
+            if ($flag_receive == 1) {
+                $follow_up = FollowUp::where('id', $id)->where('status', 1)->first();
+                if ($follow_up) {
+                    $follow_up->status = 0;
+                    $data = $follow_up->save();
+                    if ($data) {
                         return response()->json([
                             'message' => 'success',
-                            'status' => 200,
-                            'data' => $data
-                        ], 200);
-                    } elseif($data->isEmpty()) {
-                        return response()->json([
-                            'message' => 'No data found',
-                            'status' => 200,
-                            'data'=> []
-                        ], 200);
-                    }
-            //     }else{
-            //         return response()->json([
-            //                 'message' => 'Unauthenticated',
-            //                 'status' => 401
-            //             ], 401);
-            //     }
-            // }else{
-            //     return response()->json([
-            //                 'message' => 'Unauthenticated',
-            //                 'status' => 401
-            //             ], 401);
-            // }
-        
-    }
-    
-    public function change_status(Request $request,$id)
-    {
-        if($request->bearerToken()){
-                $flag = Http::withToken($request->bearerToken())->post('https://crmuser.quadque.digital/api/check-if-token-exists');
-                $flag_receive = $flag['data'];
-                if($flag_receive == 1){
-                    $follow_up = FollowUp::where('id', $id)->where('status', 1)->first();
-                    if ($follow_up) {
-                        $follow_up->status = 0;
-                        $data = $follow_up->save();
-                        if ($data) {
-                            return response()->json([
-                                'message' => 'success',
-                                'status' => 201,
-                                'data' => $follow_up
-                            ], 201);
-                        } else {
-                            return response()->json([
-                                'message' => 'failed',
-                                'status' => 500
-                            ], 500);
-                        }
+                            'status' => 201,
+                            'data' => $follow_up
+                        ], 201);
                     } else {
                         return response()->json([
-                            'message' => 'Already inactive',
-                            'status' => 200
-                        ], 404);
+                            'message' => 'failed',
+                            'status' => 500
+                        ], 500);
                     }
-                }else{
+                } else {
                     return response()->json([
-                            'message' => 'Unauthenticated',
-                            'status' => 401
-                        ], 401);
+                        'message' => 'Already inactive',
+                        'status' => 200
+                    ], 404);
                 }
-            }else{
+            } else {
                 return response()->json([
-                            'message' => 'Unauthenticated',
-                            'status' => 401
-                        ], 401);
+                    'message' => 'Unauthenticated',
+                    'status' => 401
+                ], 401);
             }
+        } else {
+            return response()->json([
+                'message' => 'Unauthenticated',
+                'status' => 401
+            ], 401);
+        }
     }
-    
+
     public function get_user_details(Request $request)
     {
         // dd($request->user_id);
@@ -397,7 +398,7 @@ class ReminderController extends Controller
             // 'data' => $follow_up
         ]);
     }
-    
+
     public function destroy($id)
     {
         $follow_up = FollowUp::find($id);
